2f36f96a53439eb413d2ed91c035d1a2128d6b34
Googler-PC <leopazhani@gmail.com>
2017-02-11 21:28:10 +0530

    Initial Commit

---
+++ MedWik.Search.Shared/Entity/SearchEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedWik.Search.Shared.Entity
+{
+    public class SearchEntity
+    {
+
+
+    }
+}
---
+++ MedWik.Search.Shared/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MedWik.Search.Shared.WebApiApplication" Language="C#" %>
